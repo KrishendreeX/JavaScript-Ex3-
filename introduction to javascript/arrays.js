ca24063d93c1f862fb30f36e57be7d803db4b715
Krishendree Kistensamy <krishendree@gmail.com>
2026-05-22 10:22:23 +0200

    Add files via upload

---
+++ introduction to javascript/arrays.js
@@ -0,0 +1,18 @@
+// TODO: Create an empty array called colors
+const colors = [];
+
+// TODO: Add three colors to the array using push()
+colors.push("red", "green", "blue");
+
+// TODO: Create another array called numbers with five numbers
+const numbers = [1, 2, 3, 4, 5];
+
+// TODO: Remove the last color from the colors array
+colors.pop();
+
+// TODO: Add a new color to the beginning of the colors array
+colors.unshift("yellow");
+
+// TODO: Print the length of both arrays
+console.log(colors.length);
+console.log(numbers.length);
